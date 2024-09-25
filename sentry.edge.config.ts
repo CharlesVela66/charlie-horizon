8c78fb9acad57d9bd5ac49a2c00ad99693d37680
Carlos <A01708634@tec.mx>
2024-09-25 17:06:43 -0600

    Add Sentry to view errors on the sentry dashboard real time

---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a89e8ff7d14abf25c89f8fd4194f4063@o4508015895379968.ingest.us.sentry.io/4508015897083904",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
